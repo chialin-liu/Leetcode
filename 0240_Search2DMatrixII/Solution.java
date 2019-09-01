dbe0dc2353efce026f31480ca419bba04bc90be3
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-01 23:45:45 +0800

    solve

---
+++ 0240_Search2DMatrixII/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while(col >= 0 && row < matrix.length){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }
+            else{
+                col--;
+            }
+                
+        }
+        return false;
+    }
+}

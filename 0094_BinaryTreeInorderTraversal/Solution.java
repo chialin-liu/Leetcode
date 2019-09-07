4b2afe946b3c1a4c6dcb6332f36ac0e2b1e9bf58
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-07 23:12:19 +0800

    solve

---
+++ 0094_BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+
+        }
+        return list;
+    }
+}

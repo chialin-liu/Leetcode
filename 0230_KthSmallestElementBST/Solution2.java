15e2902ecabea1fe1fd2148f2d823b77a21f6ef4
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-08 14:15:25 +0800

    better solved

---
+++ 0230_KthSmallestElementBST/Solution2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        if(root == null) return -1;
+        TreeNode cur = root;
+        int count = 0;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            count++;
+            if(count == k) return cur.val;
+            cur = cur.right;
+        }
+        return -1;
+    }
+}

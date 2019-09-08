d557535943e940c80f2748f047b8510ae79fc293
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-08 15:08:43 +0800

    solve

---
+++ 0285_InorderSuccessorInBST/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of the BST.
+     * @param p: You need find the successor node of p.
+     * @return: Successor of p.
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        TreeNode cur = root;
+        TreeNode lastNode = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            if(lastNode == p){
+                return cur;
+            }else{
+                lastNode = cur;
+            }
+            cur = cur.right;
+        }
+        return null;
+    }
+}

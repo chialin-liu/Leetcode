402cd23c585c7e740cef1fe489892f13badf4b7b
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-09 20:46:12 +0800

    iteration

---
+++ 0111_MinimumDepthBinaryTree/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null || root.right == null){
+            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+
+    }
+}

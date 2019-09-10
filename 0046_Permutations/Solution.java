e0db41def2446d1cfaddf8196158b1e29dcb4d44
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-10 09:00:30 +0800

    cs

---
+++ 0046_Permutations/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /*
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        // write your code here
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0) {
+            res.add(new ArrayList<Integer>());
+            return res;
+        }
+        
+        helper(nums, res, new ArrayList<Integer>());
+        return res;
+    }
+    private void helper(int []nums, List<List<Integer>> res, List<Integer> list){
+        if(list.size() == nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            helper(nums, res, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}

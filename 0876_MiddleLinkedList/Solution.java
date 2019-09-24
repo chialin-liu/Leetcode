6c4d467c11b43fe1753c0736a0ed71d44122f3b4
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-24 19:14:48 +0800

    lintcode sol

---
+++ 0876_MiddleLinkedList/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: the head node
+     * @return: the middle node
+     */
+    public ListNode middleNode(ListNode head) {
+        // write your code here.
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        // dummy.next = slow;
+        return slow;
+    }
+}

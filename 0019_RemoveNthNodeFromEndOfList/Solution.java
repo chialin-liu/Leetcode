3a8b458b0d5b2bfb348bc84614d28196c15189ba
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-09-24 09:06:28 +0800

    cs

---
+++ 0019_RemoveNthNodeFromEndOfList/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param n: An integer
+     * @return: The head of linked list.
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // write your code here
+        ListNode dummy = new ListNode(0);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        dummy.next = head;
+        if(head == null) return null;
+        for(int i = 0; i <= n; i++){
+            fast = fast.next;
+        }
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}

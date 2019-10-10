6f4fefb45cd340a60c7f41ceabe2f5b2a2676afd
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-10-10 22:02:23 +0800

    cs

---
+++ 0026_RemoveDuplicateFromSortedArray/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] != nums[i]){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}

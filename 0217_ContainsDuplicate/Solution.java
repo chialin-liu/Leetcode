192554952b1481bc577ffbbd549d05bd9a8f3ae2
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-10-20 08:14:45 +0800

    self-sol

---
+++ 0217_ContainsDuplicate/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i: nums){
+            if(set.contains(i)){
+                return true;
+            }
+            else{
+                set.add(i);
+            }
+        }
+        return false;
+    }
+}

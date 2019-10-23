906f4e96bd7bcf095fa30bc4258bfd44c917898e
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-10-23 08:48:50 +0800

    cspiration-sol

---
+++ 0032_LongestValidParenthesis/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int start = -1;
+        int res = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            }
+            else{
+                if(stack.isEmpty()){
+                    start = i;
+                }
+                else{
+                    stack.pop();
+                    if(stack.isEmpty()){
+                       res = Math.max(res, i - start); 
+                    }
+                    else{
+                        res = Math.max(res, i - stack.peek());
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}

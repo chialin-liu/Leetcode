d84e25f1203f45f98d5e83fc59946e7b44b2f5bb
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-10-24 22:37:33 +0800

    leetcode-sol

---
+++ 0671_SecondMinimumNodeInBinaryTree/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+        if(root == null) return -1;
+        if(root.left == null && root.right == null) return -1;
+        int left = root.left.val;
+        int right = root.right.val;
+        if(left == root.val){
+            left = findSecondMinimumValue(root.left);
+        }
+        if(right == root.val){
+            right = findSecondMinimumValue(root.right);
+        }
+        if(left != -1 && right != -1){
+            return Math.min(right, left);
+        }
+        else if(left == -1){
+            return right;
+        }
+        else{
+            return left;
+        }
+    }
+}

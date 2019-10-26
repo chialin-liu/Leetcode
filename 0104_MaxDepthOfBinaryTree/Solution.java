9c974662286bb1b31a3abfba824ac9055c4710e6
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-10-26 15:02:25 +0800

    cspiration-sol

--- 0104_MaxDepthOfBinaryTree/Solution.java
+++ 0104_MaxDepthOfBinaryTree/Solution.java
@@ -1,4 +1,39 @@
 /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        queue.offer(root);
+        boolean convert = true;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+                if(convert){
+                    list.add(cur.val);
+                }
+                else{
+                    list.add(0, cur.val);
+                }
+                if(cur.left != null) queue.offer(cur.left);
+                if(cur.right != null) queue.offer(cur.right);
+            }
+            res.add(list);
+            convert = !convert;
+        }
+        return res;
+    }
+}/**
  * Definition of TreeNode:
  * public class TreeNode {
  *     public int val;

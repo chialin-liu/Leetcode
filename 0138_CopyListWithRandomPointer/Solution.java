1cdca0fb6c4fd6bc88f3373ccb1c6da0b77a599c
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-12-03 20:59:58 +0800

    cspiration-sol

---
+++ 0138_CopyListWithRandomPointer/Solution.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        if(head == null) return null;
+        Node cur = head;
+        while(cur != null){
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while(cur != null){
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}

380b0b71dbdbebd66c74ece6f40e7f0d0dff82ac
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-12-06 00:52:41 +0800

    cspiration-sol

---
+++ 0166_FractionRecurringDecimal/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if(numerator == 0){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append((numerator > 0) ^ (denominator > 0)? "-": "");
+        long num = Long.valueOf(Math.abs((long)numerator));
+        long den = Long.valueOf(Math.abs((long)denominator));
+        
+        sb.append(num / den);
+        num = num % den;
+        if(num == 0){
+            return sb.toString();
+        }
+        sb.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(num, sb.length());
+        while(num != 0){
+            num = num * 10;
+            sb.append(num / den);
+            num = num % den;
+            if(map.containsKey(num)){
+                int index = map.get(num);
+                sb.insert(index, "(");
+                sb.append(")");
+                break;
+            }
+            else{
+                map.put(num, sb.length());
+            }
+        }
+        return sb.toString();
+    }
+}

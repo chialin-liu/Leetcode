363b6c6269dd30991128d248ed452ccfb4a3e45f
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-12-23 22:14:54 +0800

    self-sol

---
+++ 0364_NestedListWeightSumII/Solution.java
@@ -0,0 +1,55 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> queue = new LinkedList<>(nestedList);
+        int pre = 0;
+        int total = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            int level = 0;
+            for(int i = 0; i < size; i++){
+                NestedInteger cur = queue.poll();
+                if(cur.isInteger()){
+                    level += cur.getInteger();
+                }  
+                else{
+                    List<NestedInteger> next = cur.getList();
+                    for(NestedInteger list: next){
+                        queue.offer(list);
+                    }
+                }
+            }
+            pre += level;
+            total += pre;
+            
+        }
+        return total;
+    }
+}

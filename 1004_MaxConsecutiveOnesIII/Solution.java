c98440681e548c2e29fd95b63c8de6a72421af76
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-12-24 23:23:18 +0800

    self

--- 1004_MaxConsecutiveOnesIII/Solution.java
+++ 1004_MaxConsecutiveOnesIII/Solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public int longestOnes(int[] A, int K) {
-        Map<Integer, Integer> map = new HashMap<>();
         int start = 0;
         int end = 0;
         int count = 0;
         int max = Integer.MIN_VALUE;
         while(end < A.length){
             int cur = A[end];
-            map.put(cur, map.getOrDefault(cur, 0) + 1);
-            while(map.getOrDefault(0, 0) > K){
+            if(cur == 0){
+                count++;
+            }
+            while(count > K){
                 int begin = A[start];
                 if(begin == 0){
-                    map.put(0, map.get(0) - 1);
+                    count--;
                 }
                 start++;
             }

84d1302f369440bddc28cd708969e7955ee3dcc4
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-12-24 09:01:50 +0800

    self

---
+++ 0758_BoldWordInString/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String boldWords(String[] dict, String s) {
+        boolean []res = new boolean [s.length()];
+        int end = 0;
+        for(int i = 0; i < s.length(); i++){
+            for(String cur: dict){
+                if(s.startsWith(cur, i)){
+                    end = Math.max(end, i + cur.length());
+                }
+            }
+            res[i] = end > i;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(!res[i]){
+                sb.append(s.charAt(i));
+                continue;
+            }
+            else{
+                int j = i;
+                while(j < s.length() && res[j]){
+                    j++;
+                }
+                sb.append("<b>"+s.substring(i, j)+"</b>");
+                i = j - 1;
+            }
+        }
+        return sb.toString();
+    }
+}

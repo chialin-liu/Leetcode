79487bce55ddad91990e5a46aa16352e3accc985
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-02-02 21:24:23 +0800

    self

---
+++ 0170_TwoSumIII/Solution.java
@@ -0,0 +1,36 @@
+class TwoSum {
+
+    /** Initialize your data structure here. */
+    Map<Integer, Integer> map;
+    public TwoSum() {
+        map = new HashMap<>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for(int key: map.keySet()){
+            if(map.containsKey(value - key)){
+                if(((value - key) == key) && (map.get(key) < 2)){
+                    continue;
+                }
+                else{
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */

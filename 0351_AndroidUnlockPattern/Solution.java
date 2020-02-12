b018d096727fb480e37d2b27adf5462b64015624
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-02-12 23:09:40 +0800

    self

---
+++ 0351_AndroidUnlockPattern/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfPatterns(int m, int n) {
+        int skip[][] = new int[10][10];
+        skip[1][3] = skip[3][1] = 2;
+        skip[1][7] = skip[7][1] = 4;
+        skip[3][9] = skip[9][3] = 6;
+        skip[7][9] = skip[9][7] = 8;
+        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
+        boolean []visited = new boolean[10];
+        int res = 0;
+        for(int i = m; i <= n; i++){
+            res += dfs(visited, i - 1, skip, 1) * 4;
+            res += dfs(visited, i - 1, skip, 2) * 4;
+            res += dfs(visited, i - 1, skip, 5);
+        }
+        return res;
+    }
+    public int dfs(boolean []visited, int remain, int [][]skip, int cur){
+        if(remain == 0) return 1;
+        visited[cur] = true;
+        int res = 0;
+        for(int i = 1; i <= 9; i++){
+            if(!visited[i] && (skip[cur][i] == 0 || visited[skip[cur][i]])){
+                res += dfs(visited, remain - 1, skip, i);
+            }
+        }
+        visited[cur] = false;
+        return res;
+    }
+}
